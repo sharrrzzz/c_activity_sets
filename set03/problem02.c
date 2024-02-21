9cb8f17ee451e5392ece375e7fa5a8cc8dc3c4be
Sharanya R <sharanyar.bsc23@rvu.edu.in>
2024-02-21 20:59:01 +0530

    test

--- set03/problem02.c
+++ set03/problem02.c
@@ -26,13 +26,17 @@ void input_triangle(float *x1, float *y1, float *x2, float *y2, float *x3, float
 
 int is_triangle(float x1, float y1, float x2, float y2,float x3, float y3)
 {
-    if ((x1 * (y2-y3) + x2 * (y3 - y1) + x3 * (y2-y1)) != 0)
+    float slope1 = (y2 - y1) / (x2 - x1);
+    float slope2 = (y3 - y2) / (x3 - x2);
+    float slope3 = (y1 - y3) / (x1 - x3);
+
+    if (slope1 != slope2 && slope2 != slope3 && slope1 != slope3)
     {
         return 1;
     }
-    else 
+    else
     {
-        return 0;
+         return 0;
     }
 }
 
