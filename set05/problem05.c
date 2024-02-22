e89905da620a347758a6982d5c97cfbc5dfe9380
Sharanya R <sharanyar.bsc23@rvu.edu.in>
2024-02-23 00:21:49 +0530

    test

--- set05/problem05.c
+++ set05/problem05.c
@@ -1,11 +1,45 @@
 #include <stdio.h>
-#include <string.h>
 
-void input(char *name);
-int has_nice_name(char *name);
-void output(char *name, int res);
+int input(int x);
+float borga_X(int x);
+void output(int x, float result);
 
 int main()
 {
-    
+    int x;
+    float result;
+    x = input(x);
+    result = borga_X(x);
+    output(x, result);
+    return 0;
+}
+
+int input(int x)
+{
+    printf("Enter a number: ");
+    scanf("%d",&x);
+    return x;
+}
+
+float borga_X(int x)
+{
+    float result = 1.0;
+    float term = 1.0;
+    float factorial = 1.0;
+    float x_power = x;
+    int n = 1;
+    while (term >= 0.000001)
+    {
+        factorial = factorial * (2 * n) * (2 * n + 2);
+        term = x_power / factorial;
+        result = result + term;
+        x_power = x * x_power;
+        n++;
+    }
+    return result;
+}
+
+void output(int x, float result)
+{
+    printf("Borga(%d) = %.6f",x,result);
 }
