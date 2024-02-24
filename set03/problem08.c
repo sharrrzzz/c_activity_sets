cdaea243d56831e3a6f2a121ec59daa93c74629a
Sharanya R <sharanyar.bsc23@rvu.edu.in>
2024-02-24 23:08:26 +0530

    test

--- set03/problem08.c
+++ set03/problem08.c
@@ -2,12 +2,13 @@
 #include <stdlib.h>
 #include <math.h>
 
-typedef struct point {
-    float x,y;
+typedef struct point
+{
+    float x, y;
 } Point;
 
-
-typedef struct polygon {
+typedef struct polygon
+{
     int sides;
     Point p[100];
     float perimeter;
@@ -17,7 +18,7 @@ int input_n();
 Point input_point(char *promt_msg);
 int input_polygon(Polygon *p);
 float find_distance(Point a, Point b);
-void find_perimeter(Polygon* p);
+void find_perimeter(Polygon *p);
 void output(Polygon p);
 
 int main()
@@ -33,26 +34,26 @@ int input_n()
 {
     int n;
     printf("Enter the number of sides of the polygon: ");
-    scanf("%d",&n);
+    scanf("%d", &n);
     return n;
 }
 
 Point input_point(char *promt_msg)
 {
     Point p;
-    printf("%s\n",promt_msg);
-    scanf("%f %f",&p.x, &p.y);
+    printf("%s\n", promt_msg);
+    scanf("%f %f", &p.x, &p.y);
     return p;
 }
 
 int input_polygon(Polygon *p)
 {
     p->sides = input_n();
-    printf("Enter the coordinates of the vertices of the polygon: ");
+    printf("Enter the coordinates of the vertices of the polygon: \n");
     for (int i = 0; i < p->sides; i++)
     {
-        printf("Vertex %d: \n",i+1);
-        p->p[i] = input_point("Enter the coordinates (x,y): \n");
+        printf("Point %d: \n", i + 1);
+        p->p[i] = input_point("(x,y): ");
     }
     return p->sides;
 }
@@ -61,29 +62,28 @@ float find_distance(Point a, Point b)
 {
     float dx = b.x - a.x;
     float dy = b.y - a.y;
-    float distance = dx * dx + dy + dy;
-    float x_old=1,x_new=distance/2;
-    float precision=0.000001;
-    while(fabs(x_new-x_old)>precision)
+    float distance = dx * dx + dy * dy;
+    float x_old = 1, x_new = distance / 2;
+    float precision = 0.000001;
+    while (fabs(x_new - x_old) > precision)
     {
-        x_old=x_new;
-        x_new=(x_old+distance/x_old)/2;
+        x_old = x_new;
+        x_new = (x_old + distance / x_old) / 2;
     }
     return x_new;
 }
 
-void find_perimeter(Polygon* p)
+void find_perimeter(Polygon *p)
 {
     float perimeter = 0.0;
     for (int i = 0; i < p->sides; i++)
     {
-        perimeter = perimeter + find_distance(p->p[i], p->p[i+1 % p->sides]);
+        perimeter = perimeter + find_distance(p->p[i], p->p[(i + 1) % p->sides]);
     }
     p->perimeter = perimeter;
 }
 
 void output(Polygon p)
 {
-    printf("The perimeter of the polygon is %f",p.perimeter);
+    printf("The perimeter of the polygon is %f", p.perimeter);
 }
-
