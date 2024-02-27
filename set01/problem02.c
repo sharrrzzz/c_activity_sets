a158f8e6bd7b2b2b2748fdccfff143ea959f61ad
Sharanya R <sharanyar.bsc23@rvu.edu.in>
2024-02-27 19:52:12 +0530

    tets

--- set01/problem02.c
+++ set01/problem02.c
@@ -1,2 +1,12 @@
 // 2. Write a C program to add two numbers.
-
+#include <stdio.h>
+int main()
+{
+    int a,b;
+    printf("Enter the first number: ");
+    scanf("%d",&a);
+    printf("Enter the second number: ");
+    scanf("%d",&b);
+    int sum = a+b;
+    printf("The sum of %d and %d is %d",a,b,sum);
+}
